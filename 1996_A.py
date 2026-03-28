4270e97d19e701776203d00bdd6773d0bcaf4e53
Krishna Malani <krishnamalani77@gmail.com>
2026-03-28 23:46:10 +0530

    1996 A Solution

---
+++ 1996_A.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+for _ in range():
+    
+    n = int(input())
+    
+    count = 0
+    
+    while n >= 4:
+        n -= 4
+        count += 1
+        
+    if n == 2:
+        count += 1
+        
+    print(count)
